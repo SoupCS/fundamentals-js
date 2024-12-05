6dd21e5a9d65ef72d975b452298e3e3d7c1772d0
YoungSmoke <71344549+SoupCS@users.noreply.github.com>
2024-12-05 11:13:47 +0700

    dasdfasdzf

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const concatStrings = (a, b) => a + b;
 
+const isString = (value) => typeof value === 'string';
+
+const identifySign = (num) => {
+    if (num < 0) {
+        return 'Отрицательное число';
+    } else if (num > 0) {
+        return 'Положительное число';
+    } else {
+        return 'Ноль';
+    }
+};
+
+const reverseWords = (text) => text.split(' ').reverse().join(' ');
+
+const wordsCount = (text) => {
+    if (!text.trim()) return 0;
+    return text.split(/\s+/).length;
+};
+
+export { concatStrings, isString, identifySign, reverseWords, wordsCount };
